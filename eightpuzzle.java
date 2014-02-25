aaf469a43d1c5a9b37f82ea80b3dcdc188bbda34
Isabella Moreira <isabella@knights.ucf.edu>
2014-02-24 22:48:39 -0500

    update training

--- eightpuzzle.java
+++ eightpuzzle.java
@@ -1,101 +1,93 @@
-package mercer2010;
 import java.io.*;
 import java.util.*;
 
-class eightpuzzle {
+class EightPuzzle {
 
-	public static Queue<String> q = new LinkedList<String>();    // Use of Queue Implemented using LinkedList for Storing All the Nodes in BFS.
-	public static  Map<String,Integer> map = new HashMap<String, Integer>(); // HashMap is used to ignore repeated nodes
-	public static boolean tf;
+    Queue<String> q = new LinkedList<String>();    // Use of Queue Implemented using LinkedList for Storing All the Nodes in BFS.
+    Map<String,Integer> map = new HashMap<String, Integer>(); // HashMap is used to ignore repeated nodes
+
     public static void main(String args[]) throws IOException
     {
-        Scanner in = new Scanner(System.in);
-        int numCases = in.nextInt();
+        Scanner in = new Scanner(new File("bfs.in"));
+        int times = in.nextInt();
         
-        for(int i = 0; i < numCases; i++){        	
+        while(times > 0)
+        {
             String str = "";
-            for(int j = 0; j < 3; j++)
-            	str += (in.nextInt() + "" + in.nextInt() + "" + in.nextInt());
+            str += (in.next() + "" + in.next() + "" + in.next());
+            str = str.replace(".", "0");
         
-            //build up search space as we go, based on initial board case
-            add(str, 0);
+            System.out.println(str);
+            //String str="123485760";                                 // Input the Board State as a String with 0 as the Blank Space
+            
+            EightPuzzle e = new EightPuzzle();              // New Instance of the EightPuzzle
+            e.add(str,0);                                                   // Add the Initial State
 
-            //try all routes from this node
-            while(q.peek()!= null){
-                up(q.peek());     
-                down(q.peek());                                     
-                left(q.peek());                                     
-                right(q.remove());                          
+            while(e.q.peek()!= null){
+                
+                e.up(e.q.peek());                                       // Move the blank space up and add new state to queue
+                e.down(e.q.peek());                                     // Move the blank space down
+                e.left(e.q.peek());                                     // Move left
+                e.right(e.q.remove());                          // Move right and remove the current node from Queue
             }
-	            
-	        System.out.println(map.get(str)); //use this
+            System.out.println("-1");
+            
+            times--;
         }
     }
 
-    //add this board state and depth if we haven't already gotten here yet
-    public static void add(String str,int n){
+    //Add method to add the new string to the Map and Queue
+    void add(String str,int n){
         if(!map.containsKey(str)){
                 map.put(str,n);
                 q.add(str);
         }
     }
 
-    //move our String pieces up
-    public static void up(String str){
+    /* Each of the Methods below Takes the Current State of Board as String. Then the operation to move the blank space is done if possible.
+       After that, the new string is added to the map and queue.If it is the Goal State then the Program Terminates.
+    */
+    void up(String str){
         int a = str.indexOf("0");
-        
-        //can't be in the first row
         if(a>2){
-        	String s = str.substring(0,a-3)+"0"+str.substring(a-2,a)+str.charAt(a-3)+str.substring(a+1);
-            add(s,map.get(str)+1);
+                String s = str.substring(0,a-3)+"0"+str.substring(a-2,a)+str.charAt(a-3)+str.substring(a+1);
+                add(s,map.get(str)+1);
+                if(s.equals("123456780")) {
+                        System.out.println("Solution Exists at Level "+map.get(s)+" of the tree");
+                        //System.exit(0);
+                }
         }
     }
-    
-    //move our String pieces down
-    public static void down(String str){
+    void down(String str){
         int a = str.indexOf("0");
-      
-        //can't be in the last row
         if(a<6){
-        	String s = str.substring(0,a)+str.substring(a+3,a+4)+str.substring(a+1,a+3)+"0"+str.substring(a+4);
-            add(s,map.get(str)+1);
+                String s = str.substring(0,a)+str.substring(a+3,a+4)+str.substring(a+1,a+3)+"0"+str.substring(a+4);
+                add(s,map.get(str)+1);
+                if(s.equals("123456780")) {
+                        System.out.println("Solution Exists at Level "+map.get(s)+" of the tree");
+                        //System.exit(0);
+                }
         }
     }
-    
-    //move our String pieces left
-    public static void left(String str){
+    void left(String str){
         int a = str.indexOf("0");
-        
-        //can't be in the left-most column
         if(a!=0 && a!=3 && a!=6){
-        	String s = str.substring(0,a-1)+"0"+str.charAt(a-1)+str.substring(a+1);
-            add(s,map.get(str)+1);
+                String s = str.substring(0,a-1)+"0"+str.charAt(a-1)+str.substring(a+1);
+                add(s,map.get(str)+1);
+                if(s.equals("123456780")) {
+                        System.out.println("Solution Exists at Level "+map.get(s)+" of the tree");
+                        //System.exit(0);
+                }
         }
     }
-    
-    //move our String pieces right
-    public static void right(String str){
+    void right(String str){
         int a = str.indexOf("0");
-        
-        //can't be in the right-most row
         if(a!=2 && a!=5 && a!=8){
                 String s = str.substring(0,a)+str.charAt(a+1)+"0"+str.substring(a+2);
                 add(s,map.get(str)+1);
+                if(s.equals("123456780")) {
+                        System.out.println("Solution Exists at Level "+map.get(s)+" of the tree");
+                }
         }
     }
 }
-
-/*
-
-3 
-1 2 3 
-4 5 6 
-7 8 0 
-1 2 3 
-7 0 4 
-8 6 5 
-4 2 6 
-3 1 5 
-7 0 8
-
-*/
